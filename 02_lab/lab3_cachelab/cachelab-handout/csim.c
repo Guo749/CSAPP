f1d6797cdc3ffb8c1e07b3b8d3823e5e5845f1f3
Guo749 <venturekwok@gmail.com>
2021-11-05 18:48:36 -0400

    test

--- 02_lab/lab3_cachelab/cachelab-handout/csim.c
+++ 02_lab/lab3_cachelab/cachelab-handout/csim.c
@@ -1,7 +1,27 @@
 #include "cachelab.h"
+#include "unistd.h"
+#include "stdlib.h"
+#include "stdio.h"
 
-int main()
+int main(int argc, char** argv)
 {
+    int option;
+    int s;
+    int e;
+    int b;
+    while( (option = getopt(argc, argv, "seb")) != -1){
+        switch (option) {
+            case 's':
+                s = atoi(optarg);
+            case 'e':
+                e = atoi(optarg);
+            case 'b':
+                b = atoi(optarg);
+        }
+    }
+
+    printf("%d %d %d\n", s, e, b);
+
     printSummary(0, 0, 0);
     return 0;
 }
